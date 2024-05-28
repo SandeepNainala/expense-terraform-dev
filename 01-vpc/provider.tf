0d40e5a550dbff63227ac3e6dc3397026705d351
sandeepanainala <nainala_sandeep@yahoo.com>
2024-05-28 17:06:46 +0530

    terraform - aws vpc

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "devops91-remote-state"
+    bucket = "devops91-terraform"
     key    = "expense-dev-vpc"
     region = "us-east-1"
     dynamodb_table = "daws78s-locking"
