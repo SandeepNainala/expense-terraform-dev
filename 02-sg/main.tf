e8dee1f4c82ffeed5bb09dc2bab76cf0ae3da80a
sandeepanainala <nainala_sandeep@yahoo.com>
2025-02-11 11:37:09 +0530

    Terraform - expense project, module SG

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -2,8 +2,18 @@ module "db" {
   source = "../../terraform-aws-securitygroup/"
   project_name = var.project_name
   environment = var.environment
-  sg_description = var.db_sg_description
+  sg_description = "Security group for the database instances"
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   common_tags = var.common_tags
   sg_name = "db"
+}
+
+module "backend" {
+  source = "../../terraform-aws-securitygroup/"
+  project_name = var.project_name
+  environment = var.environment
+  sg_description = "Security group for the backend instances"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+  sg_name = "backend"
 }
