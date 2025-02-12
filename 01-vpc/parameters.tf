95cac11061285b609582a7c3d52c757a92942bc3
sandeepanainala <nainala_sandeep@yahoo.com>
2025-02-12 12:20:09 +0530

    Terraform - expense project, module SG bastion

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -3,3 +3,15 @@ resource "aws_ssm_parameter" "vpc_id" {
   type  = "String"
   value = module.vpc.vpc_id
 }
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.project_name}/${var.environment}/public_subnet_ids"  # /my-project/dev/vpc_id  # /my-project/prod/vpc_id # /my-project/stage/vpc_id
+  type  = "StringList"
+  value = module.vpc.public_subnet_ids
+}
+
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.project_name}/${var.environment}/private_subnet_ids"  # /my-project/dev/vpc_id  # /my-project/prod/vpc_id # /my-project/stage/vpc_id
+  type  = "StringList"
+  value = module.vpc.private_subnet_ids
+}
