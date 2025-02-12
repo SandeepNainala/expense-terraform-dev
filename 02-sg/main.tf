edef2754cfeb05cd51cc916dc755b1edf1260af9
sandeepanainala <nainala_sandeep@yahoo.com>
2025-02-12 11:47:10 +0530

    Terraform - expense project, module SG

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -105,3 +105,39 @@ resource "aws_security_group_rule" "frontend_public" {
   security_group_id = module.frontend.sg_id
   type              = "ingress"
 }
+
+resource "aws_security_group_rule" "frontend_bastion" {
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id = module.frontend.sg_id
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "frontend_ansible" {
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.ansible.sg_id
+  security_group_id = module.frontend.sg_id
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "bastion_public" {
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.bastion.sg_id
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "ansible_public" {
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.ansible.sg_id
+  type              = "ingress"
+}
