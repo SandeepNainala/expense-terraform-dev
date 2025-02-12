18662d63bc93936e1cdb387077e64478b030b85d
sandeepanainala <nainala_sandeep@yahoo.com>
2025-02-12 15:35:07 +0530

    Terraform - expense project, module SG bastion

--- 03-bastion/main.tf
+++ 03-bastion/main.tf
@@ -1,12 +1,16 @@
 module "bastion" {
   source = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "bastion"
+  name = "${var.environment}-${var.environment}-bastion"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [module.bastion_sg.this_security_group_id]
-  subnet_id = module.public_subnet.this_subnet_id
-
-  tags = {
-    Name = "bastion"
-  }
+  vpc_security_group_ids = [data.aws_ssm_parameter.bastion_sg_id.value]
+  # convert StringList to list and get the first element
+  subnet_id = element(split(",", data.aws_ssm_parameter.public_subnet_ids.value), 0)  # element is used to get the first element of the list, split is used to convert the string to list
+  ami = data.aws_ami.ami_info.id
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.environment}-${var.environment}-bastion"
+    }
+  )
 }
