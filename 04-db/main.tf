8a4e943dabcabddf4d74a81a9c4e0ca867a3bfde
sandeepanainala <nainala_sandeep@yahoo.com>
2025-02-12 16:11:29 +0530

    Terraform - expense project, module SG bastion

--- 04-db/main.tf
+++ 04-db/main.tf
@@ -1,34 +1,18 @@
 module "db" {
   source = "terraform-aws-modules/rds/aws"
 
-  identifier = "demodb"
+  identifier = "${var.project_name}-${var.environment}"     # This is the name of the RDS instance expense-dev
 
   engine            = "mysql"
-  engine_version    = "5.7"
-  instance_class    = "db.t3a.large"
+  engine_version    = "8.0"
+  instance_class    = "db.t3.large"
   allocated_storage = 5
 
-  db_name  = "demodb"
-  username = "user"
+  db_name  = "transaction"   # default schema for expense project
+  username = "root"
   port     = "3306"
 
-  iam_database_authentication_enabled = true
-
-  vpc_security_group_ids = ["sg-12345678"]
-
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  backup_window      = "03:00-06:00"
-
-  # Enhanced Monitoring - see example for details on how to create the role
-  # by yourself, in case you don't want to create it automatically
-  monitoring_interval    = "30"
-  monitoring_role_name   = "MyRDSMonitoringRole"
-  create_monitoring_role = true
-
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
+  vpc_security_group_ids = [data.aws_ssm_parameter.db_sg_id.value]
 
   # DB subnet group
   create_db_subnet_group = true
@@ -70,4 +54,8 @@ module "db" {
       ]
     },
   ]
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
 }
